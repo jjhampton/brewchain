2c0419611dfdc729473aa54cdb850da690d42252
John Hampton <john_j_hampton@yahoo.com>
2018-01-06 16:48:35 -0500

    Add http server

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const BrewNode = require('./brewNode');
+
+const port = 18070 + Math.floor(Math.random() * 30);
+console.log('starting node on ', port);
+
+let node1 = new BrewNode(port);
+
+node1.init();
+
+const http_port = 3000 + Math.floor(Math.random() * 10);
+
+let BrewHTTP = function() {
+    const app = new express();
+
+    app.use(bodyParser.json());
+
+    app.get('/broadcast', (req, res) => {
+        console.log(`broadcasting ${req.query.message}`);
+        node1.broadcastMessage(req.query.message);
+        res.send();
+    });
+
+    // For testing purposes - currently only uses localhost
+    app.get('/addNode/:port', (req, res) => {
+        console.log(`adding localhost host: ${req.params.port}`);
+        node1.addPeer('localhost', req.params.port);
+
+        res.send();
+    });
+
+    app.get('/spawnBrew/:teammember', (req, res) => {
+        let newBlock = node1.createBlock(req.params.teammember);
+        console.log('block created');
+        console.log(node1.getStats());
+        res.send();
+    });
+
+    app.listen(http_port, () => {
+        console.log(`http server up ... ${http_port}`);
+    });
+}
+
+let httpserver = new BrewHTTP();
